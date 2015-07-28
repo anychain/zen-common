fdfc8062fff14760dca00708936f2ceb0e071fd4
frank <frank@esse.io>
2015-07-28 23:08:27 +0800

    add thumbnail and filemagic
    Change-Id: I191bdc6cdec207f69bf9a339a062ef35d1f6c89e

---
+++ zencomm/utils/codec.py
@@ -0,0 +1,45 @@
+'''
+@copyright:      Copyright esse.io. 2015 All Rights Reserved
+@license:        Property of esse.io
+@author:         Frank Han (frank@esse.io)
+@version:        created at 2015-07-28
+
+common encoding utils
+
+'''
+
+import base64
+import zencomm.log as logging
+
+def base64_encode(src):
+    '''
+        encode src with base64
+    '''
+    result = ''
+    try:
+        result = base64.standard_b64encode(src)
+    except Exception as e:
+        logging.error('encode base64string error: [%s]' % e)
+    return result
+
+def base64_decode(src):
+    '''
+        decode base64 encoded string
+    '''
+    result = ''
+    try:
+        result = base64.standard_b64decode(src)
+    except Exception, e:
+        logging.error('decode base64string error: [%s]' % e)
+    return result
+
+if __name__ == "__main__":
+    import sys
+    input_str = "hello,world"
+    if len(sys.argv) == 2:
+        input_str = sys.argv[1]
+    print "Source string is %s" % input_str
+    b64str = base64_encode(input_str)
+    print "After base64 encode, the string is %s" % b64str
+    source_str = base64_decode(b64str)
+    print "After base64 decode, the source string is %s" % source_str
---
+++ zencomm/utils/filemagic.py
@@ -0,0 +1,34 @@
+'''
+@copyright:      Copyright esse.io. 2015 All Rights Reserved
+@license:        Property of esse.io
+@author:         Frank Han (frank@esse.io)
+@version:        created at 2015-07-28
+
+get file metadata
+
+'''
+
+import magic
+import zencomm.log as logging
+
+
+def get_file_type(filename):
+    '''
+        @return: images types like image/jpeg, image/png
+    '''
+
+    try:
+        ftype = magic.from_file(filename, mime = True)
+    except Exception as e:
+        logging.error("failed to file type for file %s with error: %s"
+                      % (filename, e))
+    return ftype
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print "please input the file to check"
+        sys.exit(1)
+    filename = sys.argv[1]
+    print get_file_type(filename)

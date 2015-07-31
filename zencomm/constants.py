70f3e7291e79f73e3c124d64c646101250093d18
gary <gary@esse.io>
2015-07-31 15:44:41 +0800

    add addr code
    Change-Id: I3fc6637f7ad56592784acbd6e03044582fa6d08d

--- zencomm/constants.py
+++ zencomm/constants.py
@@ -1,6 +1,11 @@
-#define constants
+# constants definition
 
 # 1) Data Dictionary Const
 DATA_DICT_STATE = 1
 DATA_DICT_TAG = 2
 DATA_DICT_TYPE = 3
+
+# 2) Address Code Region Level
+ADDR_CODE_PROVINCE = '1'
+ADDR_CODE_CITY = '2'
+ADDR_CODE_COUNTY = '3'

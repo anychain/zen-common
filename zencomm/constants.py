b77d65bfb6a9845c5994e526eb52d27407492723
gary <gary@esse.io>
2015-08-01 17:04:51 +0800

    refine constant
    Change-Id: I2188b96297016f061ece0462c8c8f198c54ef7ec

--- zencomm/constants.py
+++ zencomm/constants.py
@@ -3,7 +3,8 @@
 # 1) Data Dictionary Const
 DATA_DICT_STATE = 1
 DATA_DICT_TAG = 2
-DATA_DICT_TYPE = 3
+DATA_DICT_ORG_TYPE = 3
+DATA_DICT_BRANCH_TYPE = 4
 
 # 2) Address Code Region Level
 ADDR_CODE_PROVINCE = '1'

30fa55f041d973e13cfaa0cb8d812491b46ad0b3
frank <frank@esse.io>
2015-08-02 17:56:09 +0800

    refine zen doc
    Change-Id: Ibab5c932b5dd6a0639c080d0fbc060546afc4db9

--- zencomm/api/api_request.py
+++ zencomm/api/api_request.py
@@ -5,7 +5,7 @@
 import json
 
 from zencomm.api import constants as CONST
-
+from zencomm.log import logger
 
 class APIReq():
     """Base zen api request
@@ -47,6 +47,7 @@ class APIReq():
                                                                self.resource)
                     break
         if errmsg:
+            logger.error("Api request validation failed with error %s" % errmsg)
             return False, errmsg
         else:
             return True, "The api request is valid"
--- zencomm/log/__init__.py
+++ zencomm/log/__init__.py
@@ -1,1 +1,17 @@
-from logging import *
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger('zen')
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    logger.info('test logger_info')
+    logger.warn('test logger_warn')
+    logger.error('test logger_error')
+    logger.debug('test logger_debug')
